cf7a4873b08dcaa9d937f938d8540fd31d26a0b4
Eman3600 <real.eman3600@gmail.com>
2024-12-02 23:23:26 -0500

    Added Extensible Content

---
+++ script.js
@@ -0,0 +1,20 @@
+// List of background images
+const images = [
+    "background/x-tile.png",
+    "background/o-tile.png",
+    "background/d-tile.png",
+    "background/p-tile.png",
+    "background/r-tile.png",
+    "background/g-tile.png"
+];
+
+// Select the background container
+const background = document.querySelector('.background');
+
+// Generate tiles for the background
+for (let i = 0; i < 7000; i++) { // Adjust for number of tiles
+    const tile = document.createElement('div');
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+    tile.style.backgroundImage = `url(${randomImage})`;
+    background.appendChild(tile);
+}
